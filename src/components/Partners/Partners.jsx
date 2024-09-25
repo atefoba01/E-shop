230288eb261e35a764fefed1117ee65997ab6461
Emmanuel Agboola <emmanuel.a@nextbewe.com>
2024-09-25 11:30:55 +0100

    first commit

---
+++ src/components/Partners/Partners.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Partners = () => {
+  return (
+    <div>
+      partner
+    </div>
+  )
+}
+
+export default Partners
---
+++ src/components/Popup/Popup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Popup = () => {
+  return (
+    <div>
+      pop <i class="fa fa-upload" aria-hidden="true"></i>
+    </div>
+  )
+}
+
+export default Popup
